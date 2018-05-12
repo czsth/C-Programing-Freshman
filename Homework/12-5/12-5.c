4a12befeccec8ac7093b0547c12f89fe3870a028
czsth <34812091+czsth@users.noreply.github.com>
2018-05-12 22:55:59 +0800

    roll back

--- Homework/12-5/12-5.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "isPalindrome.h"
-
-int main(int argc, char const *argv[]) {
-    int rsl = isPalindrome("abcdcba");	//rsl refers to "result"
-    printf("%d\n", rsl);
-    return 0;
-}
--- Homework/12-5/isPalindrome.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string.h>
-
-int isPalindrome(const char * str);
-int isPalindrome_internal(const char * head, const char * tail);
